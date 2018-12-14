e2524d4250795d81460f11c39a8d3d705b44b583
Asher Matheson <ashrlmthsn@gmail.com>
2018-12-14 20:37:40 +1300

    Setup homepage

---
+++ app/src/main/java/ashrlm/cardsagainstsociety/homepage.java
@@ -0,0 +1,23 @@
+package ashrlm.cardsagainstsociety;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+    public void startGame(View view) {
+        //TODO: Setup start game (Seperate activity
+    }
+
+    public void joinGame(View view) {
+        //TODO: Setup join game (Seperate activity)
+    }
+}
